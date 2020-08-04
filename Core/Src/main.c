c4a005e3499a5657e8b3916ee62788e732dd3da1
MattKefford <kefford7@gmail.com>
2020-08-04 09:49:58 +0100

    Added test new line
    Just a test

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,6 +23,8 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
+// Test new line
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
